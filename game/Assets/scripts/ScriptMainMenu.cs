f1787c6271b1e381ce80095f1e990f3b8d8101f7
oleg <okusch9@gmail.com>
2015-10-14 20:10:18 +0300

    файл с игрой
    Signed-off-by: oleg <okusch9@gmail.com>

---
+++ game/Assets/scripts/ScriptMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptMainMenu : MonoBehaviour {
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (150, 10, 100, 50), "Start")) 
+		{
+		print ("Start game");
+			Application.LoadLevel("Adventure_level_1");
+		}
+
+		if (GUI.Button (new Rect (450, 10, 100, 50), "Exit")) 
+		{
+		print ("Exit");
+		Application.Quit();
+		}
+	}
+}
